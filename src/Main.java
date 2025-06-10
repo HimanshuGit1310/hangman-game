8ee081257b41e5c47d16c4194f8ce0858474cd77
HimanshuGit1310 <himanshujhariyazeal@gmail.com>
2025-06-10 20:43:18 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         String word = "pizza";
         char guess ;
+        int wrongGuess = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,15 +21,96 @@ public class Main {
         System.out.println("*****************************");
         System.out.println();
 
-        System.out.print("Word : ");
+       while (wrongGuess < 6) {
+           System.out.print("Word : ");
 
-        for (char c : wordState) {
-            System.out.print(c + " ");
-        }
+           for (char c : wordState) {
+               System.out.print(c + " ");
+           }
+           System.out.println();
+
+           System.out.print("Enter a Guess: ");
+           guess = scanner.next().toLowerCase().charAt(0);
+
+           if (word.indexOf(guess) >= 0) {
+               System.out.println("Correct Guess");
+
+               for (int i = 0 ; i < word.length() ; i++){
+                   if (word.charAt(i) == guess){
+                       wordState.set(i,guess);
+                   }
+               }
+
+               if (!wordState.contains('_')) {
+                   System.out.println("You Won");
+                   System.out.println("Game is Over");
+                   break;
+               }
+           }
+           else {
+               System.out.println("Wrong guess !!!");
+               wrongGuess++;
+
+               if (wrongGuess >= 6){
+                   System.out.println("Game is Over");
+                   System.out.println("Correct word is: " + word);
+               }
+           }
 
 
+
+
+       }
 
         scanner.close();
 
+    }
+
+    static String getHandmanArt(int wrongGuess) {
+        return switch (wrongGuess) {
+            case 0 -> """
+                    
+                    
+                    
+                    """;
+
+            case 1 -> """
+                        o
+                    
+                    
+                    """;
+
+            case 2 -> """
+                        o
+                        |
+                    
+                    """;
+
+            case 3 -> """
+                        0
+                       /|
+                    
+                    """;
+
+            case 4 -> """
+                        o
+                       /|\\
+                    
+                    """;
+
+            case 5 -> """
+                        o
+                       /|\\
+                       /
+                    """;
+
+            case 6 -> """
+                        o
+                       /|\\
+                       / \\
+                    """;
+
+            default -> "";
+        };
     }
 }
